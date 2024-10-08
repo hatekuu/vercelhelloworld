109f3cc418ce6ef723bf21996096ba1ed78b591f
unknown <hatekuu@gmail.com>
2024-10-08 12:08:48 +0700

    dong

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
-// server.js
 const express = require('express');
-const { MongoClient } = require('mongodb');
-const authRoutes = require('./controllers/authController');
-
-require('dotenv').config();
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-// Middleware
+// Middleware để phân tích body JSON
 app.use(express.json());
 
-// Kết nối tới MongoDB
-const client = new MongoClient(process.env.MONGO_URI);
-client.connect(err => {
-    if (err) {
-        console.error('Failed to connect to the database:', err);
-        return;
-    }
-    console.log('Connected to MongoDB');
-});
-
-// Đường dẫn xác thực
-app.use('/auth', authRoutes(client));
-
-// Đường dẫn Hello World
+// Định nghĩa một route đơn giản
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-// Khởi động server
+// Bắt đầu server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
